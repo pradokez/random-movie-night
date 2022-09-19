45e7cdbc3e6cf27578c60b1f5436e268167e57e9
pradokez <pradokeziah@gmail.com>
2022-09-19 19:23:47 -0300

    misc

---
+++ src/RandomMovieGenerator.jsx
@@ -0,0 +1,26 @@
+import "./RandomMovieGenerator.css";
+import popcorn from "./popcorn.png";
+
+function RandomMovieGenerator() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">
+          <h1>Random Movie Night</h1>
+          <img className="popcorn" src={popcorn} alt="popcorn with sunglasses"></img>
+        </header>
+        <div className="movie-content">
+          <span className="streaming" hidden></span>
+          <span className="genre" hidden></span>
+          <span hidden>O filme é</span>
+          <img className="poster" hidden alt="movie poster"/>
+          <h2 className="title" hidden></h2>
+          <p className="overview" hidden></p>
+        </div>
+      </div>
+      <div className="bar" />
+    </div>
+  );
+}
+
+export default RandomMovieGenerator;
