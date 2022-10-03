ef3a03295e4e4b220641843bda7ee98a25e9a161
pradokez <pradokeziah@gmail.com>
2022-10-03 19:08:28 -0300

    fix various issues and get it running properly

--- src/RandomMovieGenerator.jsx
+++ src/RandomMovieGenerator.jsx
@@ -2,22 +2,55 @@ import "./styles/RandomMovieGenerator.css";
 import popcorn from "./popcorn.png";
 import Button from "./components/Button";
 import { useState, useEffect } from "react";
-import CreateMovieDatabase from "./components/MovieDatabase";
 
-// TODO: show genre on screen
-// TODO: hide API key
-// TODO: show year
-// TODO: set up start button
+// TODO: turn movie infos into card with props
 
 const RandomMovieGenerator = () => {
   const [movie, setMovie] = useState();
   const [poster, setPoster] = useState(movie);
+  const [title, setTitle] = useState(movie);
+  const [year, setYear] = useState(movie);
+  const [overview, setOverview] = useState(movie);
+  
   const [started, setStarted] = useState(false);
 
+  const options = {
+    method: "GET",
+    headers: {
+      'X-RapidAPI-Key': 'e3d485b5d9msh3e3a2f06d0f912ep111791jsnd7e62dbf62bb',
+      "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+    },
+  };
+  function randomize(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  const streamings = ["disney", "netflix", "prime", "hbo"];
+  const randomStreaming = streamings[randomize(streamings)];
+  const randomPage = Math.floor(Math.random() * 200);
+
   useEffect(() => {
-    const pickRandomMovie = async () => {};
+    const pickRandomMovie = async () => {
+        try {
+          const response = await fetch(
+            `https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=${randomStreaming}&type=movie&page=${randomPage}&output_language=en&language=en`,
+            options
+          );
+          const data = await response.json();
+          const results = await data.results;
+          setMovie(results[randomize(results)]);
+          console.log(movie, poster) 
+          setPoster(movie.posterURLs.original);
+          setTitle(movie.title);
+          setYear(movie.year);
+          setOverview(movie.overview);
+        } catch (err) {
+          console.error(err);
+        }
+       
+    };
     pickRandomMovie();
-  }, [setMovie]);
+  }, [started]);
 
   return (
     <div className="App">
@@ -35,26 +68,21 @@ const RandomMovieGenerator = () => {
 
         {started && (
           <div className="movie-content">
-            <span className="streaming"></span>
+            <span className="streaming">{randomStreaming}</span>
             <img
               className="poster"
               alt="movie poster"
-              src="https://m.media-amazon.com/images/M/MV5BOTJiYjBhZDgtMjhiOC00MTIzLThlNGMtMmI1NjIwM2M3YTI5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
+              src={poster}
             />
-          </div>
+          </div> 
         )}
       </div>
       {!started && <Button action={()=>{setStarted(true)}} name="Start" />}
       <div className="bar">
         {started && (
           <div className="movie-content">
-            <h2 className="title">The Mummy (1999)</h2>
-            <p className="overview">
-              At an archaeological dig in the ancient city of Hamunaptra, an
-              American serving in the French Foreign Legion accidentally awakens
-              a mummy who begins to wreak havoc as he searches for the
-              reincarnation of his long-lost love.
-            </p>
+            <h2 className="title">{`${title} (${year})`}</h2>
+            <p className="overview">{overview}</p>
           </div>
         )}
       </div>
